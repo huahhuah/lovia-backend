32e355bf0b4491d18edfe7a3101d63b76b70e066
蘇祺元 <suqiyuan@suqiyuande-MacBook-Pro.local>
2025-05-05 02:43:13 +0800

    新增提案填寫

--- routes/projects.js
+++ routes/projects.js
@@ -4,7 +4,10 @@ const config = require("../config/index");
 const { dataSource } = require("../db/data-source");
 const logger = require("../utils/logger");
 const projects = require("../controllers/projects");
+const auth = require("../middlewares/auth");
+const { createproject } = require("../controllers/projects");
 
 router.get("/:project_id", projects.getProject);
+router.post("/create", createproject);
 
 module.exports = router ;
