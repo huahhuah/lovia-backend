38ac9d03ee07ed837a28b3181e9d59b4e99e518b
huahhuah <huah734@gmail.com>
2025-05-04 23:04:49 +0800

    fix: 改為支援 DATABASE_URL 以便 Render 使用

--- db/data-source.js
+++ db/data-source.js
@@ -9,11 +9,25 @@ const Roles = require("../entities/Roles");
 const Statuses = require("../entities/Statuses");
 const Project_plans = require("../entities/Project_plans");
 
+// 判斷是否存在 DATABASE_URL（Render 環境會提供）
+const useUrl = !!process.env.DATABASE_URL;
+
 const dataSource = new DataSource({
   type: "postgres",
-  url: config.get("db.url"), // ✅ 改為 url
+  ...(useUrl
+    ? {
+        url: process.env.DATABASE_URL,
+        ssl: config.get("db.ssl")
+      }
+    : {
+        host: config.get("db.host"),
+        port: Number(config.get("db.port")),
+        username: config.get("db.username"),
+        password: config.get("db.password"),
+        database: config.get("db.database"),
+        ssl: config.get("db.ssl")
+      }),
   synchronize: config.get("db.synchronize"),
-  ssl: config.get("db.ssl"),
   entities: [Users, Projects, Categories, Genders, Roles, Statuses, Project_plans]
 });
 
