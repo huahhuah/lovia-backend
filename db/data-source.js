cb1f7c32ce42d9e1e69078904e16d3bda461467a
huahhuah <huah734@gmail.com>
2025-05-06 22:04:38 +0800

    feat: 新增會員查詢API;移除重複資料表;修正提案亂碼問題

--- db/data-source.js
+++ db/data-source.js
@@ -9,7 +9,6 @@ const Genders = require("../entities/Genders.js");
 const Roles = require("../entities/Roles.js");
 const Statuses = require("../entities/Statuses.js");
 const Project_plans = require("../entities/Project_plans.js");
-const CreateProjects = require("../entities/CreateProjects.js");
 
 const isRender = process.env.DATABASE_URL?.includes("render.com");
 const sslOption = isRender || config.get("db.ssl") ? { rejectUnauthorized: false } : false;
@@ -20,7 +19,7 @@ const dataSource = new DataSource({
   synchronize: config.get("db.synchronize"),
   poolSize: 10,
   ssl: sslOption,
-  entities: [Users, Projects, Categories, Genders, Roles, Statuses, Project_plans, CreateProjects]
+  entities: [Users, Projects, Categories, Genders, Roles, Statuses, Project_plans]
 });
 
 module.exports = { dataSource };
