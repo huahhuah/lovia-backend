5c40e6019d296f8213c8d3769e2e39e8668e3cd4
huahhuah <huah734@gmail.com>
2025-05-17 09:58:20 +0800

    feat(api):新增探索API新增探索API

--- routes/upload.js
+++ routes/upload.js
@@ -1,41 +1,41 @@
-const express = require('express');
-const multer = require('multer');
-const uploadImg = require('../services/uploadImg');
-const appError = require('../utils/appError')
+const express = require("express");
+const multer = require("multer");
+const uploadImg = require("../services/uploadImg");
+const appError = require("../utils/appError");
 const router = express.Router();
-const logger = require ('../utils/logger')("UploadRoute");
+const logger = require("../utils/logger")("UploadRoute");
 
 const upload = multer({
-    storage: multer.memoryStorage(),
-    // 限制上傳檔案最大為2MB
-    limits:{
-        fileSize: 2*1024*1024
-    }
+  storage: multer.memoryStorage(),
+  // 限制上傳檔案最大為2MB
+  limits: {
+    fileSize: 2 * 1024 * 1024
+  }
 });
 const IMGBB_API_KEY = process.env.IMGBB_API_KEY;
 
-router.post('/image', upload.single('file'), async (req, res, next) =>{
-    try {
-        if (!req.file?.buffer){
-            return next(appError(400, '未上傳檔案'));
-        }
-        console.log(req.file); // 確認你有拿到檔案
-        const result = await uploadImg(req.file.buffer, IMGBB_API_KEY);
-        res.json({
-            status: 'success',
-            message: '圖片上傳成功',
-            url: result.url,
-            type: req.body.type,
-            filename: result.title || result.id,
-            size_kb: (req.file.size/1024).toFixed(1),
-        });
-    } catch (error){
-        logger.error("上傳失敗", error);
-        if (error.code === 'LIMIT_FILE_SIZE'){
-            return next(appError(400, '檔案大小不能超過 2MB'));
-        }
-        next(error);
+router.post("/image", upload.single("file"), async (req, res, next) => {
+  try {
+    if (!req.file?.buffer) {
+      return next(appError(400, "未上傳檔案"));
     }
+    console.log(req.file); // 確認你有拿到檔案
+    const result = await uploadImg(req.file.buffer, IMGBB_API_KEY);
+    res.json({
+      status: "success",
+      message: "圖片上傳成功",
+      url: result.url,
+      type: req.body.type,
+      filename: result.title || result.id,
+      size_kb: (req.file.size / 1024).toFixed(1)
+    });
+  } catch (error) {
+    logger.error("上傳失敗", error);
+    if (error.code === "LIMIT_FILE_SIZE") {
+      return next(appError(400, "檔案大小不能超過 2MB"));
+    }
+    next(error);
+  }
 });
 
 module.exports = router;
--- services/uploadImg.js
+++ services/uploadImg.js
@@ -1,42 +1,44 @@
 // 對Imgbb溝通的邏輯
 // upload to imgbb
-const axios = require ('axios');
-const FormData = require('form-data');
-const imageType = require('image-type'); // 判斷圖片格式
-const appError = require('../utils/appError');
+const axios = require("axios");
+const FormData = require("form-data");
+const imageType = require("image-type"); // 判斷圖片格式
+const appError = require("../utils/appError");
 
 async function uploadImg(imageBuffer, apiKey) {
-    try {
-        console.log('🧪 Uploading image with apiKey:', apiKey); //檢查確認
-        // 使用image-type
-        const type = imageType(imageBuffer);
-        if (!type || (type.mime !== 'image/jpeg' && type.mime !== 'image/png')) {
-            throw appError(400, '只接受 JPG 或 PNG 格式的圖片');
-        }
-        const filename = `image.${type.ext}`;
-        const contentType = type.mime;
-        // 建立FormData 把圖片數據加進來
-        const form = new FormData();
-        form.append('key', apiKey);
-        form.append('image', imageBuffer, {
-            filename: filename,
-            contentType: contentType,
-        });
-
-        // 發POST 到ImgBB 的API
-        const res = await axios.post('https://api.imgbb.com/1/upload', form, {
-            headers: {
-                ...form.getHeaders(),
-            },
-        });
-        // 返回上傳的圖片資料
-        return res.data.data;
-    } catch (error) {
-        if(error.response){
-            throw appError(500, `ImgBB API問題 ${error.response.data.message}`)
-        }else{
-            throw appError(400, error.message || '圖片上傳失敗')
-        }
+  try {
+    console.log("🧪 Uploading image with apiKey:", apiKey); //檢查確認
+    console.log("📦 buffer length:", imageBuffer.length);
+    // 使用image-type
+    const type = imageType(imageBuffer);
+    console.log("🔍 Detected image type:", type);
+    if (!type || (type.mime !== "image/jpeg" && type.mime !== "image/png")) {
+      throw appError(400, "只接受 JPG 或 PNG 格式的圖片");
     }
+    const filename = `image.${type.ext}`;
+    const contentType = type.mime;
+    // 建立FormData 把圖片數據加進來
+    const form = new FormData();
+    form.append("key", apiKey);
+    form.append("image", imageBuffer, {
+      filename: filename,
+      contentType: contentType
+    });
+
+    // 發POST 到ImgBB 的API
+    const res = await axios.post("https://api.imgbb.com/1/upload", form, {
+      headers: {
+        ...form.getHeaders()
+      }
+    });
+    // 返回上傳的圖片資料
+    return res.data.data;
+  } catch (error) {
+    if (error.response) {
+      throw appError(500, `ImgBB API問題 ${error.response.data.message}`);
+    } else {
+      throw appError(400, error.message || "圖片上傳失敗");
+    }
+  }
 }
 module.exports = uploadImg;
