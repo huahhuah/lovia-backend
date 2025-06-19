de48db2f6feb2f6b9b8fb4f01f368de4413e8df3
PJA0103 <anne0103@outlook.com>
2025-06-19 14:25:20 +0800

    add project status enetiy and revise related document

---
+++ entities/Project_statuses.js
@@ -0,0 +1,26 @@
+// 提案審查:defaul 1 (1-審查中、2-提案通過、3-提案駁回)
+const { EntitySchema } = require("typeorm");
+
+module.exports = new EntitySchema({
+  name: "ProjectStatuses",
+  tableName: "PROJECT_STATUSES",
+  columns: {
+    id: {
+      primary: true,
+      type: "int",
+      generated: "increment"
+    },
+    status: {
+      type: "varchar",
+      length: 20,
+      nullable: false
+    }
+  },
+  relations: {
+    projects: {
+      type: "one-to-many",
+      target: "Projects",
+      inverseSide: "projectStatus"
+    }
+  }
+});
