cbd8073409695b8065218ca816e5be8c272d7c81
PJA0103 <anne0103@outlook.com>
2025-06-22 21:38:04 +0800

    revise put controller

--- controllers/projects.js
+++ controllers/projects.js
@@ -9,6 +9,7 @@ const { v4: uuidv4 } = require("uuid");
 const { In } = require("typeorm");
 const Project = require("../entities/Projects");
 const ProjectComments = require('../entities/Project_comments');
+const { PlainObjectToNewEntityTransformer } = require("typeorm/query-builder/transformer/PlainObjectToNewEntityTransformer.js");
 
 //  步驟一：建立專案
 async function createProject(req, res, next) {
@@ -251,7 +252,18 @@ async function updateProject(req, res, next) {
     // 更新有變更的欄位
     if (title !== undefined) project.title = title;
     if (summary !== undefined) project.summary = summary;
-    if (category !== undefined) project.category = category.name; //  這裡
+    // 這裡
+    if (category !== undefined) {
+      const categoryRepo = dataSource.getRepository("Categories");
+      const newCategory = await categoryRepo.findOne({
+        where : {id: category.id}
+      });
+      if (!newCategory) return next(appError(400, '無效的分類', next));
+      project.category = newCategory;
+      console.log(newCategory);
+    }     
+
+    //  這裡
     if (total_amount !== undefined) project.total_amount = Number(total_amount);
     if (start_time !== undefined) project.start_time = start_time;
     if (end_time !== undefined) project.end_time = end_time;
@@ -277,10 +289,11 @@ async function updateProject(req, res, next) {
           feedback: plan.feedback,
           feedback_img: plan.feedback_img,
           delivery_date: plan.delivery_date,
-          project_id: { id: projectId }
+          project: { id: projectId }
         });
       });
       await planRepo.save(newPlans);
+      console.log(newPlans);
     }
     const resData = {
       title: project.title,
@@ -292,10 +305,10 @@ async function updateProject(req, res, next) {
       cover: project.cover,
       full_content: project.full_content,
       project_team: project.project_team,
-      faq: project.faq || [],
+      faq: project.faq ? JSON.parse(project.faq) : [],
       newPlans
     };
-    const updateProject = await projectRepo.save(project);
+    await projectRepo.save(project);
     res.status(200).json({
       status: true,
       data: resData
