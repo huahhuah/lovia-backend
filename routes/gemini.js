0b9209da38922e704f23e4af374a6c653bc4301a
huahhuah <149953060+huahhuah@users.noreply.github.com>
2025-07-07 19:56:27 +0800

    Update gemini.js

--- routes/gemini.js
+++ routes/gemini.js
@@ -5,5 +5,6 @@ const rateLimiter = require("../middlewares/rateLimiter");
 
 const router = express.Router();
 router.post("/", rateLimiter, geminiChat);
+router.post("", rateLimiter, geminiChat); 
 
 module.exports = router;
