b386edeb2bf01572068d1e04ee50efdab1e74f16
yu <huah734@gmail.com>
2025-07-11 00:25:35 +0800

    fix: add httpsAgent to ignore self-signed cert

--- services/uploadImg.js
+++ services/uploadImg.js
@@ -3,32 +3,36 @@
 const axios = require("axios");
 const FormData = require("form-data");
 const imageType = require("image-type"); // 判斷圖片格式
+const https = require("https");
 const appError = require("../utils/appError");
 
+const agent = new https.Agent({
+  rejectUnauthorized: false
+});
+
 async function uploadImg(imageBuffer, apiKey) {
-
-    try {
-        // 使用image-type
-        const type = imageType(imageBuffer);
-        if (!type || (type.mime !== 'image/jpeg' && type.mime !== 'image/png')) {
-            throw appError(400, '只接受 JPG 或 PNG 格式的圖片');
-        }
-        const filename = `image.${type.ext}`;
-        const contentType = type.mime;
-        // 建立FormData 把圖片數據加進來
-        const form = new FormData();
-        form.append('key', apiKey);
-        form.append('image', imageBuffer, {
-            filename: filename,
-            contentType: contentType,
-        });
-
+  try {
+    // 使用image-type
+    const type = imageType(imageBuffer);
+    if (!type || (type.mime !== "image/jpeg" && type.mime !== "image/png")) {
+      throw appError(400, "只接受 JPG 或 PNG 格式的圖片");
+    }
+    const filename = `image.${type.ext}`;
+    const contentType = type.mime;
+    // 建立FormData 把圖片數據加進來
+    const form = new FormData();
+    form.append("key", apiKey);
+    form.append("image", imageBuffer, {
+      filename: filename,
+      contentType: contentType
+    });
 
     // 發POST 到ImgBB 的API
     const res = await axios.post("https://api.imgbb.com/1/upload", form, {
       headers: {
         ...form.getHeaders()
-      }
+      },
+      httpsAgent: agent
     });
     // 返回上傳的圖片資料
     return res.data.data;
